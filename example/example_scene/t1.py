d6823d0962b13607eeba9800d741d58bae029733
jtech <javatechs@instance-20250106-180810-mendocino.local>
2025-09-25 08:06:43 -0700

    Latest changes

--- example/example_scene/t1.py
+++ example/example_scene/t1.py
@@ -2,42 +2,54 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
-from px4_msgs.msg._vehicle_status import VehicleStatus
+
+# PX4 Messages
+from px4_msgs.msg import OffboardControlMode, TrajectorySetpoint, VehicleStatus, VehicleGlobalPosition
+from px4_msgs.srv import VehicleCommand
+
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
 
-class MinimalPySubscriber(Node):
+class DroneControl(Node):
     def __init__(self):
-        super().__init__('minimal_py_subscriber')
+        super().__init__('DroneControl')
         qos_profile = QoSProfile(
             reliability=ReliabilityPolicy.BEST_EFFORT,
             history=HistoryPolicy.KEEP_LAST,
             depth=1
         )
-
-        self.subscriber_1 = self.create_subscription(
+        #
+        vehicle_status_ = None
+        self.vehicle_status_sub = self.create_subscription(
             VehicleStatus,
             '/fmu/out/vehicle_status',
-            self.listener_callback,
+            self.vehicle_status_cb,
+            qos_profile)
+        #
+        self.vehicle_gp_ = None
+        self.vehicle_global_pos_sub = self.create_subscription(
+            VehicleGlobalPosition,
+            '/fmu/out/vehicle_global_position',
+            self.vehicle_global_pos_cb,
             qos_profile)
 
-    def listener_callback(self, msg):
-        self.get_logger().info(f'I heard: "{msg}"')
-#        self.get_logger().info(f'I heard: "{msg.data}"')
-#        print(f'I heard: "{msg}"')
+    def vehicle_status_cb(self, msg):
+#        self.get_logger().info(f'I heard: "{msg}"')
+        self.vehicle_status_ = msg
+
+    def vehicle_global_pos_cb(self, msg):
+#        self.get_logger().info(f'I heard: "{msg}"')
+        self.vehicle_gp_ = msg
+    
 
 def main(args=None):
     rclpy.init(args=args)
-    print ("HERE2")
-    minimal_py_subscriber = MinimalPySubscriber()
+    node = DroneControl()
     print ("HERE3")
-#    rclpy.spin_once(minimal_py_subscriber)
-    print ("HERE3a")
-    rclpy.spin(minimal_py_subscriber)
+ #   if node.
+    rclpy.spin(node)
     print ("HERE4")
-    minimal_py_subscriber.destroy_node()
+    node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
-    print ("HERE")
     main()
